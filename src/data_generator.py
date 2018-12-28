22b4a57197ab755404b491f1986b6dfb540f71d5
tongwu <tongwu.github@outlook.com>
2018-12-29 07:05:35 +0800

    1229-1

--- src/data_generator.py
+++ src/data_generator.py
@@ -12,40 +12,60 @@ from keras.utils import to_categorical
 
 from config import train_image_folder, train_label_file, batch_size, img_width, img_height, num_classes
 
+image_folder = train_image_folder
+annot_file = train_label_file
+            
+samples = []
+with open(annot_file, 'r') as file:
+    samples = [x for x in list(csv.reader(file))[1:] if x[1] != "new_whale"]
+    # samples = [x for x in list(csv.reader(file))[1:]]
+            
+d_dic = {}
+for s in samples:
+    cid = s[1]
+    if cid in d_dic:
+        d_dic[cid].append(s)
+    else:
+        d_dic[cid] = [s]
+
+samples = []
+classes = []
+for cid in d_dic.keys():
+    if len(d_dic[cid]) > 10:
+        new_items = [(s[0], cid) for s in d_dic[cid]]
+        samples += new_items
+        classes.append(cid)
+
+
 class DataGenSequence(Sequence):
     def __init__(self, usage):
         self.usage = usage
         self.image_folder = train_image_folder
-        annot_file = train_label_file
-            
-        with open(annot_file, 'r') as file:
-            self.samples = [x for x in list(csv.reader(file))[1:] if x[1] != "new_whale"]
-            
+        
+        
+        self.train_samples = samples
         if self.usage != 'train':
-            self.samples = random.sample(self.samples, 2000)
+            self.train_samples = random.sample(samples, 200)
+        self.c2id = dict((c, i) for i, c in enumerate(classes))
 
-        self.classes = list(set([x[1] for x in self.samples]))
-        self.classes.sort()
-        self.c2id = dict((c, i) for i, c in enumerate(self.classes))
-
-        print len(self.samples)
-        print len(self.classes)
-        np.random.shuffle(self.samples)
+        print(len(self.train_samples))
+        print(len(classes))
+        np.random.shuffle(self.train_samples)
 
     def __len__(self):
-        return len(self.samples) // batch_size
+        return len(self.train_samples) // batch_size
         
     def on_epoch_end(self):
-        np.random.shuffle(self.samples)
+        np.random.shuffle(self.train_samples)
 
     def __getitem__(self, idx):
         i = idx * batch_size
-        length = min(batch_size, (len(self.samples) - i))
+        length = min(batch_size, (len(self.train_samples) - i))
         batch_inputs = np.empty((length, img_height, img_width, 3), dtype=np.float32)
         batch_target = np.empty((length, num_classes), dtype=np.float32)
 
         for i_batch in range(length):
-            sample = self.samples[i + i_batch]
+            sample = self.train_samples[i + i_batch]
             image_id = sample[0]
             filename = os.path.join(self.image_folder, image_id)
             class_id = sample[1]
@@ -54,6 +74,7 @@ class DataGenSequence(Sequence):
             image = cv.resize(image, (img_width, img_height), interpolation = cv.INTER_CUBIC)
             image = image[:, :, ::-1] # RGB
 
+            # print(image_id, ", ", self.c2id[class_id])
             batch_inputs[i_batch] = preprocess_input(image)
             batch_target[i_batch] = to_categorical(self.c2id[class_id], num_classes)
 
@@ -63,7 +84,7 @@ def revert_pre_process(x):
     return ((x + 1) * 127.5).astype(np.uint8)
 
 if __name__ == '__main__':
-    data_gen = DataGenSequence('train')
+    data_gen = DataGenSequence('t1rain')
     item = data_gen.__getitem__(0)
     x, y = item
     print(x.shape)
--- src/train.py
+++ src/train.py
@@ -5,15 +5,15 @@ from keras.callbacks import ReduceLROnPlateau
 
 from config import batch_size, patience, num_train_samples, num_valid_samples, num_epochs, verbose
 from data_generator import DataGenSequence
-from model import build_model
+from model import build_model, build_model_VGG
 
 tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
 early_stop = EarlyStopping('val_acc', patience=patience)
 reduce_lr = ReduceLROnPlateau('val_acc', factor=0.5, patience=int(patience / 4), verbose=1)
-trained_models_path = 'models/model'
+trained_models_path = '../models/model'
 model_names = trained_models_path + '.{epoch:02d}-{val_acc:.4f}.hdf5'
 model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', verbose=1, save_best_only=True)
-callbacks = [tensor_board, early_stop, reduce_lr]
+callbacks = [tensor_board, model_checkpoint, early_stop, reduce_lr]
 
 new_model = build_model()
 sgd = keras.optimizers.SGD(lr=2.5e-5, momentum=0.9, decay=1e-6, nesterov=True)
@@ -26,7 +26,7 @@ new_model.fit_generator(
         validation_steps=num_valid_samples // batch_size,
         shuffle=True,
         epochs=num_epochs,
-        initial_epoch=0,
+        initial_epoch=0, 
         callbacks=callbacks,
         verbose=verbose,
         use_multiprocessing=False)
