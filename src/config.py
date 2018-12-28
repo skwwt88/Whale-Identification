3deaddfd9e62ac004d52fbd5e7a0d74e94bcc31b
tongwu <tongwu.github@outlook.com>
2018-12-29 06:46:20 +0800

    Revert "change ignore scope"
    This reverts commit 33937bc2b78dd3b0c2214f5ec1f9908efb71d600.

--- src/config.py
+++ src/config.py
@@ -4,17 +4,17 @@ img_width, img_height = 299, 299
 num_channels = 3
 
 batch_size = 16
-num_epochs = 100
+num_epochs = 1
 patience = 50
 verbose = 1
-num_train_samples = 4354
-num_valid_samples = 200
+num_train_samples = 15697
+num_valid_samples = 2000
 
 train_image_folder = '../data/train'
 train_label_file = '../data/train.csv'
 
 FREEZE_LAYERS = 2
 dropout_rate = 0.2
-num_classes = 227
+num_classes = 5005
 
 
--- src/model.py
+++ src/model.py
@@ -1,7 +1,5 @@
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Activation
-from keras.engine.topology import Input
-from keras.layers import BatchNormalization, Concatenate, Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras.models import Model
 
 from config import img_height, img_width, num_channels, num_classes, FREEZE_LAYERS, dropout_rate
@@ -17,62 +15,7 @@ def build_model():
     x = Activation('softmax', name='Predictions')(x)
     model = Model(inputs=base_model.input, outputs=x)
     for layer in model.layers[:FREEZE_LAYERS]:
-        layer.trainable = True
+        layer.trainable = False
     for layer in model.layers[FREEZE_LAYERS:]:
         layer.trainable = True
-
     return model
-
-def build_model_VGG(with_dropout=True):
-    kwargs     = {'activation':'relu', 'padding':'same'}
-    conv_drop  = 0.2
-    dense_drop = 0.5
-    img_shape  = (img_height, img_width, num_channels)
-    inp        = Input(shape=img_shape)
-
-    x = inp
-
-    x = Conv2D(64, (9, 9), **kwargs)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = BatchNormalization()(x)
-    if with_dropout: x = Dropout(conv_drop, noise_shape=(None, 1, 1, int(x.shape[-1])))(x)
-
-    x = Conv2D(64, (2, 2), **kwargs, strides=2)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = BatchNormalization()(x)
-    if with_dropout: x = Dropout(conv_drop, noise_shape=(None, 1, 1, int(x.shape[-1])))(x)
-
-    x = Conv2D(64, (2, 2), **kwargs, strides=2)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = BatchNormalization()(x)
-    if with_dropout: x = Dropout(conv_drop, noise_shape=(None, 1, 1, int(x.shape[-1])))(x)
-
-    x = Conv2D(64, (2, 2), **kwargs, strides=2)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = BatchNormalization()(x)
-    if with_dropout: x = Dropout(conv_drop, noise_shape=(None, 1, 1, int(x.shape[-1])))(x)
-
-    x = Conv2D(64, (2, 2), **kwargs, strides=2)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = BatchNormalization()(x)
-    if with_dropout: x = Dropout(conv_drop, noise_shape=(None, 1, 1, int(x.shape[-1])))(x)
-
-    x = Conv2D(64, (2, 2), **kwargs, strides=2)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = Conv2D(64, (3, 3), **kwargs)(x)
-    x = BatchNormalization()(x)
-    if with_dropout: x = Dropout(conv_drop, noise_shape=(None, 1, 1, int(x.shape[-1])))(x)
-
-    x = GlobalAveragePooling2D()(x)
-    x = Dropout(dense_drop, name='Dropout')(x)
-    x = Dense(num_classes, name='Logits')(x)
-    x = Activation('softmax', name='Predictions')(x)
-    model = Model(inputs=inp, outputs=x)
-
-    return model
-if __name__ == '__main__':
-    build_model_VGG().summary()
